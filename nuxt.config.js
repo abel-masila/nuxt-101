c0e687a67b0cf38aff99e7fe54e8628dcd8c343f
Abel Masila <abelmasila@gmail.com>
2020-08-15 16:42:51 +0300

    made api requests with axois and added custom error handler

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,4 +64,5 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+  devTools: true,
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="">
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard'
 export default {
+  components: {
+    EventCard,
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:4000/events')
+      .then((res) => {
+        return {
+          events: res.data,
+        }
+      })
+      .catch((e) =>
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try later',
+        })
+      )
+  },
   head() {
     return {
       title: 'Event Listing',
